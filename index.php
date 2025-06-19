1613f78440a1da180c75dc7dd6caa00d77ae323a
unknown <mohanned.fathy1998@gmail.com>
2025-06-19 08:20:33 +0200

    retriving back to index.php

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
-// test output
-echo "<p>✅ PHP is executing!</p>";
 session_start();
 error_reporting(0);
 include('includes/config.php');
