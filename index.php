d7db4806bc2480a703aa165045fc3ca5d0afdb21
unknown <mohanned.fathy1998@gmail.com>
2025-06-19 07:43:37 +0200

    chore: enable PHP error reporting test in index.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+// test output
+echo "<p>✅ PHP is executing!</p>";
 session_start();
 error_reporting(0);
 include('includes/config.php');
